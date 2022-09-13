127c62103bf97595fc073bd9620e25849718b55a
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2022-09-14 01:07:53 +0530

    declare global css styles and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLink from "./components/ImageLink/ImageLink";
+import FaceDetect from "./components/FaceDetect/FaceDetect";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      boxes: [],
+      route: "signin",
+    };
+  }
+
+  displayFaceBox = (boxes) => {
+    console.log(boxes);
+    this.setState({ boxes: boxes });
+  };
+
+  calculateFaceLocation = (result) => {
+    console.log(result);
+    const clarifaiFaces = result.outputs[0].data.regions.map(
+      (region) => region.region_info.bounding_box
+    );
+    const image = document.querySelector("#input-image");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    const boxes = clarifaiFaces.map((clarifaiFace) => {
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - clarifaiFace.right_col * width,
+        bottomRow: height - clarifaiFace.bottom_row * height,
+      };
+    });
+    this.displayFaceBox(boxes);
+  };
+
+  fetchData = () => {
+    const USER_ID = "vishwas_123";
+    const PAT = "66090fdf83e14d509c71fe13b037d26a";
+    const APP_ID = "bff0a5af8ec9411d8ef77bd3f7ba363f";
+    const MODEL_ID = "face-detection";
+    const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
+    const IMAGE_URL = this.state.input;
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+    fetch(
+      "https://api.clarifai.com/v2/models/" +
+        MODEL_ID +
+        "/versions/" +
+        MODEL_VERSION_ID +
+        "/outputs",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => this.calculateFaceLocation(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    this.fetchData();
+  };
+
+  onRouteChange = (page) => {
+    this.setState({ route: page });
+  };
+
+  render() {
+    let { imageUrl, boxes, route } = this.state;
+    if (route === "signin") {
+      return (
+        <div>
+          <Logo />
+          <SignIn onRouteChange={this.onRouteChange} />
+        </div>
+      );
+    } else if (route === "register") {
+      return (
+        <div>
+          <Logo />
+          <Register onRouteChange={this.onRouteChange} />
+        </div>
+      );
+    } else if (route === "home") {
+      return (
+        <div className="center">
+          <div className="home-navigation">
+            <Logo />
+            <Navigation onRouteChange={this.onRouteChange} />
+          </div>
+          <ImageLink
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
+          />
+          <FaceDetect imageUrl={imageUrl} boxes={boxes} />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
