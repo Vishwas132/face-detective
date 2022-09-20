1def04316834e05e16af132e02b20498b8aba900
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2022-09-20 22:44:31 +0530

    Request backend to signin the user
    On clicking the submit button email and password are saved as a
    state and registerUser() function is called which sends the request to
    the backend to signin the user.
    If the the credentials match the database, route is changed to homepage.
    Otherwise the response is an error.

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,44 +1,78 @@
 import React from "react";
 import "./SignIn.css";
 
-const SignIn = ({ onRouteChange }) => {
-  return (
-    <div className="signin-container">
-      <div className="card signin-card">
-        <form action=""></form>
-        <h2 className="input-margin">Log-In</h2>
-        <input
-          className="input-box input-margin"
-          type="email"
-          id="email"
-          placeholder="email"
-        />
-        <input
-          className="input-box input-margin"
-          type="password"
-          id="password"
-          placeholder="password"
-        />
-        <button
-          className="button button-size input-margin"
-          type="button"
-          id="sign-in"
-          onClick={() => onRouteChange("home")}
-        >
-          Sign-In
-        </button>
-        <h2 className="input-margin">New user?</h2>
-        <button
-          className="button button-size input-margin"
-          type="button"
-          id="register"
-          onClick={() => onRouteChange("register")}
-        >
-          Register
-        </button>
+class SignIn extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  changeEmailState = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  changePasswordState = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  signinUser = (event) => {
+    fetch("http://localhost:3001/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.state),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.id) {
+          this.props.onRouteChange("home");
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  render() {
+    return (
+      <div className="signin-container">
+        <div className="card signin-card">
+          <h2 className="input-margin">Log-In</h2>
+          <input
+            className="input-box input-margin"
+            type="email"
+            id="email"
+            placeholder="email"
+            onChange={this.changeEmailState}
+          />
+          <input
+            className="input-box input-margin"
+            type="password"
+            id="password"
+            placeholder="password"
+            onChange={this.changePasswordState}
+          />
+          <button
+            className="button button-size input-margin"
+            type="button"
+            id="sign-in"
+            onClick={this.signinUser}
+          >
+            Sign-In
+          </button>
+          <h2 className="input-margin">New user?</h2>
+          <button
+            className="button button-size input-margin"
+            type="button"
+            id="register"
+            onClick={() => this.props.onRouteChange("register")}
+          >
+            Register
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SignIn;
